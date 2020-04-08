9c19c00b2a5b9a37fa2f9066eb0a54627f44123b
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2020-04-08 14:33:29 -0400

    Improvements on welcome + added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/App.css';
+import Navbar from './containers/Navbar';
 import Welcome from './containers/Welcome';
 
 export default class App extends React.Component{
   render(){
     return (
       <div className="App">
+        <Navbar />
         <Welcome />
       </div>
     );
--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../styles/Navbar.css';
 
 export default class Welcome extends React.Component{
     render(){return(
-        <div>
-
-        </div>
+        <nav className="navbar-container">
+            <img src="./images/navbar/pharma_icon.svg" alt="logo" className="n-logo" />
+            <div className="n-items">
+                <a href="#welcome">Welcome</a>
+                <a href="#about">About us</a>
+                <a href="#sponsors">Sponsors</a>
+                <a href="#schedule">Schedule</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </nav>
     )}
 }
