3753f60fb687e258027ab09c53566ac646b523c6
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2020-05-01 21:40:20 -0400

    Sponsor page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import Navbar from './containers/Navbar';
 import Welcome from './containers/Welcome';
 import About from './containers/About';
+import Sponsors from './containers/Sponsors';
 
 export default class App extends React.Component{
   render(){
@@ -11,6 +12,7 @@ export default class App extends React.Component{
         <Navbar />
         <Welcome />
         <About />
+        <Sponsors />
       </div>
     );
   }
