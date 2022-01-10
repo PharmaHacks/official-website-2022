d35251a5db3f5449c97f3f52dd7f709e82adeb93
conniaren <conniaren@hotmail.com>
2022-01-10 00:47:50 -0500

    Finish contact page, changes to button, index, and add accordion transition

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/Button.css';
 
-export default class Welcome extends React.Component{
+export default class Button extends React.Component{
     render(){return(
         <a className={"button-container "+this.props.color} href={this.props.action}>
             {this.props.value}
--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Contact.css';
 import Button from '../components/Button';
+import Social from '../components/Social';
 
 export default class Contact extends React.Component{
     render(){
@@ -15,7 +16,22 @@ export default class Contact extends React.Component{
                     </div>
                 </div>
                 <div className= "contact-information-section">
-
+                    <div className = "contact-information-address">
+                        <span>Trottier Building, McGill University</span>
+                        <span>3630 Rue University,</span>
+                        <span>Montréal, QC H2A 2B3</span>
+                    </div>
+                    <div className= "contact-information-email">
+                        <span><a href="mailto:contact@pharmahacks.com">contact@pharmahacks.com</a></span>
+                    </div>
+                    <div className = "contact-information-social">
+                        <span>Follow us</span>
+                        <div className= "contact-information-social-media">
+                        <Social value="/images/contact/facebook.png" action="https://www.facebook.com/StudentPharma/"/>
+                        <Social value="/images/contact/instagram.png" action="https://www.instagram.com/pharma.hacks/"/>
+                        <Social value="/images/contact/linkedin.png" action="https://www.linkedin.com/company/studentpharma/"/>
+                        </div>
+                    </div>
                 </div>
 
             </div>
