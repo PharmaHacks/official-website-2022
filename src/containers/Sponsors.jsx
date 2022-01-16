cc590dfe6f160daae508ee66248b214a95ec22f2
conniaren <conniaren@hotmail.com>
2022-01-16 02:22:17 -0500

    Small changes to formatting

--- src/containers/Sponsors.jsx
+++ src/containers/Sponsors.jsx
@@ -11,7 +11,7 @@ export default class Sponsors extends React.Component{
             <div className="sponsors-text">
                 <span className="sponsors-title">Sponsors</span>
                 <div className="sponsors-details">Our events are made possible thanks to sponsors. As a sponsor, you’ll have the opportunity to present a challenge, have your own sponsorship booth, host workshops, and/or recruit talented individuals. We offer a platform where sponsors can observe first hand the growth of our participants throughout the weekend and recruit exceptional hackers.
-                    <span>If you want to support us and bring students together for this year’s hackathon, check out our packages</span> <a href = "/Sponsorship-Package.pdf" target="_blank" rel="noopener noreferrer">here.</a></div>
+                    <span> If you want to support us and bring students together for this year’s hackathon, check out our packages</span> <a href = "/Sponsorship-Package.pdf" target="_blank" rel="noopener noreferrer">here.</a></div>
             </div>
             <div className="individual-sponsor">
                 <div className="year-titles" id = "current">Current</div>
--- src/containers/Welcome.jsx
+++ src/containers/Welcome.jsx
@@ -25,7 +25,10 @@ export default class Welcome extends React.Component{
 
             </div>
             <div className="w-title-section">
-                <img src="./images/welcome/pharma_logo_2022_location.svg" alt="Pharmahacks 2022" className="w-title-text-svg" /> <br/>
+            <picture>
+                <source srcset="./images/welcome/pharma_logo_2022.svg" media="(max-width: 600px)"/>
+                <img src="./images/welcome/pharma_logo_2022_location.svg" alt="Pharmahacks 2022" className="w-title-text-svg" /> 
+            </picture><br/>
                 <Button value="Apply now" color="red" action="https://forms.gle/WrFMJEzFrfpFfFbQ8" />
             </div>
             <img className="w-dna-gif" src="./images/welcome/DNA.gif" alt="DNA gif" />
