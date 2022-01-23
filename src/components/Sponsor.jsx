66ebcf5fca843e89c73b04b577adb4c54e5c6b7e
conniaren <conniaren@hotmail.com>
2022-01-23 16:16:18 -0500

    acadmic and sponsor colours/order

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -10,15 +10,15 @@ export default class Sponsor extends React.Component{
                 "G":"#FFD700",
                 "S":"#C0C0C0",
                 "B": "#cd7f32",
-                "O": "#000D1B",
-                "P": "rgba(66, 0, 255, 0.5)",
+                "O": "rgba(66, 0, 255, 0.5)",
+                "P": "rgb(225, 55, 55)",
             },
             tierNames:{
                 "G":"Gold",
                 "S":"Silver",
                 "B":"Bronze",
                 "O":"Startup",
-                "P":"Pharma",
+                "P":"Academic",
             }
         }
     }
